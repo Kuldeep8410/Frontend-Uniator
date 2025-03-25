e5b14a67e8b943acc79d82102c9ae4e2bd6d0d03
Kuldeep8410 <kuldeep8410mtr@gmail.com>
2025-03-25 15:25:04 +0530

    same

--- src/Teacher-pages/AdminPro.jsx
+++ src/Teacher-pages/AdminPro.jsx
@@ -11,7 +11,7 @@ function AdminPro() {
             <div className="m-2 text-6xl p-2 font-bold border-2 border-blue-500 rounded-full bg-gradient-to-tl from-red-400 to-blue-600 bg-clip-text text-transparent">
               {AdminInfo.name[0]}
             </div>
-            <div className="flex flex-col mt-7 text-white font-bold ">
+            <div className="flex flex-col mt-7 text-white font-bold overflow-hidden ">
                 <h1>{AdminInfo.name}</h1>
                 <h1 className="overflow-hidden ">{AdminInfo.email}</h1>
             </div>
