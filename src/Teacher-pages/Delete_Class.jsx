752d2937f3e191d78fd2577e37c1dc464e7ce5d9
Kuldeep8410 <kuldeep8410mtr@gmail.com>
2025-03-27 12:35:56 +0530

    deletion route mein dikkt thi

--- src/Teacher-pages/Delete_Class.jsx
+++ src/Teacher-pages/Delete_Class.jsx
@@ -1,18 +1,21 @@
 import { useEffect, useState } from "react";
 import { RiDeleteBin5Fill } from "react-icons/ri";
 import { ToastContainer,toast } from "react-toastify";
+import Loader from "../UiComponents/Loader";
 
-const BASE_URl = "http://localhost:3000/mern-revision/v1"
+const BASE_URl = "https://uniator-backend.onrender.com/mern-revision/v1"
 
 function DeleteCourse({ props }) {
 
     const [courseIs, setCourseId] = useState(null);
+    const [loading, setLoading] = useState(false);
     useEffect (() =>{
         setCourseId(props)
     },[])
     
     const deleteCourse = async (courseIs) => {        
         try {
+            setLoading(true);
             const response = await fetch(`${BASE_URl}/delete-course/${props}`, {
                 method: "DELETE",
                 credentials: "include",
@@ -30,6 +33,7 @@ function DeleteCourse({ props }) {
             if(result.success){
                 toast.success(result.message);
             }
+            setLoading(false);
         } catch (error) {
             console.error("Error deleting course:", error);
         }
@@ -38,10 +42,12 @@ function DeleteCourse({ props }) {
 
     return (
         <div>
-            <div className=" relative flex border-2 border-red-600  rounded-2xl h-10 text-white text-2xl font-bold justify-center content-center hover:bg-amber-800">
-            <button onClick={deleteCourse} className="bg-red-600 rounded-2xl"><RiDeleteBin5Fill /></button>
-            
-        </div>
+            {loading ? (<Loader />) : (
+                <div className=" relative flex border-2 border-red-600  rounded-2xl h-10 text-white text-2xl font-bold justify-center content-center hover:bg-amber-800">
+                <button onClick={deleteCourse} className="bg-red-600 rounded-2xl"><RiDeleteBin5Fill /></button>
+             
+             </div>
+            )}
         <ToastContainer />
         </div>
     )
