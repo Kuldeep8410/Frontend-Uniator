6977d343e23a0818a658fba3173c42b47767100c
Kuldeep8410 <kuldeep8410mtr@gmail.com>
2025-03-31 11:37:28 +0530

    simple name change

--- src/Landingpage/Connects.jsx
+++ src/Landingpage/Connects.jsx
@@ -10,7 +10,7 @@ function Connects() {
     return (
         <div className="flex flex-col bg-black text-white h-full justify-center content-center items-center mt-10">
             <h1 className="p-5 text-2xl text-gray-300 md:text-5xl antialiased">
-                Connect With Me Community !!
+                Connect With My Community !!
             </h1>
             <div className="relative grid grid-cols-2 grid-rows-2 gap-4 md:grid-cols-4 md:grid-rows-1 w-3/4">
 
@@ -18,7 +18,7 @@ function Connects() {
                 <div className="flex flex-col items-center justify-center w-full aspect-square bg-white/10 text-white text-lg font-semibold rounded-lg transition-all duration-300 hover:border-2 hover:border-white">
                     <img src={X} alt="Description" className="w-1/4 h-1/4 rounded-2xl" />
                     <h1>
-                        <a href="#" className="hover:underline">LinkedIn</a>
+                        <a href="#" className="hover:underline">Twitter</a>
                     </h1>
                 </div>
 
@@ -26,7 +26,7 @@ function Connects() {
                 <div className="flex flex-col items-center justify-center w-full aspect-square bg-white/10 text-white text-lg font-semibold rounded-lg transition-all duration-300 hover:border-2 hover:border-white">
                     <img src={Git} alt="Description" className="w-1/4 h-1/4 rounded-2xl" />
                     <h1>
-                        <a href="#" className="hover:underline">LinkedIn</a>
+                        <a href="#" className="hover:underline">Github</a>
                     </h1>
                 </div>
 
@@ -40,7 +40,7 @@ function Connects() {
                 <div className="flex flex-col items-center justify-center w-full aspect-square bg-white/10 text-white text-lg font-semibold rounded-lg transition-all duration-300 hover:border-2 hover:border-white">
                     <img src={Insta} alt="Description" className="w-1/4 h-1/4 rounded-2xl" />
                     <h1>
-                        <a href="#" className="hover:underline">LinkedIn</a>
+                        <a href="#" className="hover:underline">Instagram</a>
                     </h1>
                 </div>
 
