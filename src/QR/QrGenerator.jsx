53e5a00a2cbc9e00473c924dc06d6f488e27c251
Kuldeep8410 <kuldeep8410mtr@gmail.com>
2025-04-02 12:41:43 +0530

    jhghkjghj

--- src/QR/QrGenerator.jsx
+++ src/QR/QrGenerator.jsx
@@ -52,7 +52,7 @@ const QrGenerator = () => {
       {text && (
         <div>
           {/* Attach ref to the QR Code SVG */}
-          <QRCode value={text} size={256} ref={svgRef} bgColor={"#FFFFFF"} fgColor={"#FFFFFF"}/>
+          <QRCode value={text} size={256} ref={svgRef} bgColor={"#000000"} fgColor={"#FFFFFF"}/>
         </div>
       )}
 
