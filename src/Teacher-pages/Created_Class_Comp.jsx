934460bdcdf0a2a244972ddafffe240d256a35eb
Kuldeep8410 <kuldeep8410mtr@gmail.com>
2025-04-06 13:09:46 +0530

    xscvnmk

--- src/Teacher-pages/Created_Class_Comp.jsx
+++ src/Teacher-pages/Created_Class_Comp.jsx
@@ -62,7 +62,7 @@ function ClassComponent({ course }) {
     
         {/* Attendance Button */}
         <button
-            className={`${isStart ? "bg-green-500" : "bg-red-600"} w-full sm:w-1/2 p-2 mx-auto rounded-2xl bg-green-700 font-bold mt-3`}
+            className={`${isStart ? "bg-red-500" : "bg-green-600"} w-full sm:w-1/2 p-2 mx-auto rounded-2xl bg-green-700 font-bold mt-3`}
             onClick={AttendanceHandler}
         >
             {isStart ? "Stop Attendance" : "Start Attendance"}
