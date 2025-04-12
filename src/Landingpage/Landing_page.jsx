b36570a2ec239b8ea7d0d4db21602b47a431a4f6
Kuldeep8410 <kuldeep8410mtr@gmail.com>
2025-04-12 17:19:22 +0530

    responsibe logo

--- src/Landingpage/Landing_page.jsx
+++ src/Landingpage/Landing_page.jsx
@@ -28,12 +28,14 @@ function LandingPage() {
     return (
         <div className="relative flex flex-col bg-black content-center justify-center items-center h-auto">
             <div className="flex flex-col justify-between items-center content-center">
-                <h1 className="text-white antialiased text-3xl md:text-6xl p-10">
-                    <span className="text-red-500 font-bold">Welcome !</span> to <span className="bg-gradient-to-r from-[#05e8e1] via-blue-500 to-pink-500 bg-clip-text text-transparent font-bold antialiased">
-                        smvDe<span className="text-7xl font-serif  ">X</span>
+                <h1 className="text-white antialiased text-2xl sm:text-3xl md:text-5xl lg:text-6xl p-4 sm:p-6 md:p-10 text-center">
+                    <span className="text-red-500 font-bold">Welcome !</span> to
+                    <span className="bg-gradient-to-r from-[#05e8e1] via-blue-500 to-pink-500 bg-clip-text text-transparent font-bold antialiased ml-2">
+                        smvDe
+                        <span className="text-4xl sm:text-5xl md:text-7xl font-serif text-pink-400">X</span>
                     </span>
-
                 </h1>
+
                 <h1 className="text-gray-400 text-xl antialiased  text-center md:ml-50 md:mr-50 ml-10 mr-10">A versatile web platform integrating <span className="text-yellow-300">real-time code collaboration</span>, a secure gate pass system with QR scanning, an <span className="text-pink-600">attendance tracker</span>, an <span className="text-green-500">advanced to-do</span> list with backend support, and a <span className="text-teal-300">community discussion</span> forum. It streamlines workflow, enhances security, boosts productivity, and fosters collaboration, making it an all-in-one solution for developers, students, and organizations.</h1>
             </div>
             {popup && (<Pop_up close={() => setPopup(false)} />)}
