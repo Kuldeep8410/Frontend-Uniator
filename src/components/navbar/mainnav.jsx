a2342fc3dd4066b0943145c76fe5d3d24bebb1c7
KULDEEP SHARMA <118438689+Kuldeep8410@users.noreply.github.com>
2025-07-11 16:37:03 +0530

    Update mainnav.jsx

--- src/components/navbar/mainnav.jsx
+++ src/components/navbar/mainnav.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import { BiExit } from 'react-icons/bi';
 import CollaborativeEditorLanding from '../../Landingpage/part2';
 import CategoryCards from './categories';
-import MakePosts from '../make-post';
+// import MakePosts from '../make-post';
 
 
 
@@ -115,7 +115,7 @@ export default function Mainnav({ FetchAllPost, Explore, Makepost }) {
             {toggle === "search" ? (<SearchFunction />) : ""}
             {toggle === "home" ? (<CollaborativeEditorLanding />) : ""}
             {toggle === "categories" ? (<CategoryCards />) : ""}
-            {toggle === "add new" ? (<MakePosts />) : ""}
+{/*             {toggle === "add new" ? (<MakePosts />) : ""} */}
             {toggle === "message" ? (<FetchAllPost />) : ""}
             {toggle === "posts" ? (<FetchAllPost />) : ""}
 
